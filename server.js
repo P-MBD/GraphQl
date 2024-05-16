59ffd2b738217c2913d1b35fc29b132e4dab8f3d
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-05-16 11:10:18 +0330

    work with model and add fake data

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const faker = require('faker');
+const User = require('./model/users');
+const Article = require('./model/articles');
+const Comment = require('./model/comments');
+
 const app = express();
+
 mongoose.connect('mongodb://127.0.0.1/graphql-project');
 let schema = buildSchema(`
-
     type Query {
         user : User
+        FakeData : String
     }
     type User {
         fname : String
@@ -16,27 +22,40 @@ let schema = buildSchema(`
         gender : Gender
         email : String
         password : String
-        posts : [Post]
-    }
-   type Post {
+        comments : [Comment]
+     }
+    type Post {
         user : ID
         title : String
         body : String
-   } 
-   enum Gender {
-    Male
-    Female
-   }
-   input CreateUser {
-    fname : String
-    lname : String
-    age : Int
-    gender : Gender
-    email : String
-    password : String
-   }
-`)
+    }
+    enum Gender {
+        Male
+        Female
+    }
+    type Comment {
+        user : ID
+        article : ID
+        title : String
+        body : String
+    }
 
+    type Article {
+        user : ID
+        title : String
+        body : String
+    }
+    input CreateUser {
+        fname : String
+        lname : String
+        age : Int
+        gender : Gender
+        email : String
+        password : String
+    }
+
+    `)
+    
 let resolver = {
     user : () => {
         return {
@@ -44,11 +63,25 @@ let resolver = {
             lname : "kiani"
 
         }
+    },
+    FakeData : () => {
+        const addUser = new User({
+            fname : faker.name.firstName(),
+            lname : faker.name.lastName(),
+            age : faker.random.number(),
+            email : faker.internet.email(),
+            password : faker.internet.password(),
+
+        })
+        addUser.save();
+        return "data store ..."
     }
 }
 app.use('/graphql', graphqlHTTP({
-    schema : schema,
-    rootValue : resolver,
-    graphiql : true
- }))
+   schema : schema,
+   rootValue : resolver,
+   graphiql : true
+}))
+
+
 app.listen(3000, () => {console.log('server run on port 3000 ...')});
